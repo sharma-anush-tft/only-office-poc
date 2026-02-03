98638bbe67056b77890786859158b3b5ce5eef2e
Anush-Admin <anush-admin@AnushS-MacBook-Air-2.local>
2026-02-03 12:18:48 +0530

    fix

--- routes/onlyofficeRoutes.js
+++ routes/onlyofficeRoutes.js
@@ -13,11 +13,11 @@ router.get("/file", (req, res) => {
 
     res.setHeader(
       "Content-Type",
-      "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
     );
     res.setHeader(
       "Content-Disposition",
-      'attachment; filename="demo-cim.docx"'
+      'attachment; filename="demo-cim.docx"',
     );
 
     res.sendFile(filePath);
@@ -27,11 +27,10 @@ router.get("/file", (req, res) => {
   }
 });
 
-
 router.get("/file1", async (req, res) => {
   try {
     const url =
-"https://boosst-apim.azure-api.net/test/api/onlyoffice/file?subscription-key=0ffea418a5d24b1d956419d812eda5e4&fileLink=https%3A%2F%2Fboosstblobstorage.blob.core.windows.net%2Fonlyofficedocx%2Fc4620784-4241-4925-abf0-9c6a6a4b8ba3.docx%3Fsv%3D2025-11-05%26spr%3Dhttps%26se%3D2026-02-02T14%253A27%253A39Z%26sr%3Db%26sp%3Drl%26sig%3DjFsy%252BeFOp1QEN6aaf7evxZSWAHTTTI7HM0hBSc5ws%252Fg%253D"
+      "https://boosst-apim.azure-api.net/test/api/onlyoffice/file?subscription-key=0ffea418a5d24b1d956419d812eda5e4&fileLink=https%3A%2F%2Fboosstblobstorage.blob.core.windows.net%2Fonlyofficedocx%2Fc4620784-4241-4925-abf0-9c6a6a4b8ba3.docx%3Fsv%3D2025-11-05%26spr%3Dhttps%26se%3D2026-02-02T14%253A27%253A39Z%26sr%3Db%26sp%3Drl%26sig%3DjFsy%252BeFOp1QEN6aaf7evxZSWAHTTTI7HM0hBSc5ws%252Fg%253D";
     const response = await axios.get(url, {
       responseType: "stream",
       timeout: 15000,
@@ -39,14 +38,14 @@ router.get("/file1", async (req, res) => {
 
     res.setHeader(
       "Content-Type",
-      "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
     );
     res.setHeader(
       "Content-Disposition",
-      'attachment; filename="demo-cim.docx"'
+      'attachment; filename="demo-cim.docx"',
     );
 
-    response.data.pipe(res); 
+    response.data.pipe(res);
   } catch (error) {
     console.error("REMOTE FILE ERROR:", error.message);
     res.status(500).json({
@@ -56,28 +55,34 @@ router.get("/file1", async (req, res) => {
   }
 });
 
-
 router.get("/config", (req, res) => {
   const baseUrl = `https://only-office-poc-production.up.railway.app`;
   const { username, userid } = req.query;
 
   const filePath = path.join(__dirname, "../assets/demo-cim.docx");
   const stat = fs.statSync(filePath);
-  const version = stat.mtimeMs;   // version
+  const version = stat.mtimeMs;
 
-  const key = `doc-${version}`;   // ← versioned key
+  const key = `doc-${version}`;
 
   const config = {
     document: {
       fileType: "docx",
       title: "demo-cim.docx",
       url: `${baseUrl}/api/v1/onlyoffice/file`,
-      key,   
+      key,
+      permissions: {
+        edit: true,
+        download: true,
+      },
     },
     documentType: "word",
     editorConfig: {
       mode: "edit",
       callbackUrl: `${baseUrl}/api/v1/onlyoffice/callback`,
+      events: {
+        onDocumentReady: "onOnlyOfficeReady",
+      },
       user: {
         id: userid || "guest",
         name: username || "Guest User",
@@ -85,7 +90,7 @@ router.get("/config", (req, res) => {
       customization: {
         uiTheme: "default-dark",
         plugins: false,
-        forcesave: true
+        forcesave: true,
       },
     },
   };
@@ -94,12 +99,12 @@ router.get("/config", (req, res) => {
   res.json({ config, token });
 });
 
-
 router.post("/callback", async (req, res) => {
   try {
     const body = req.body;
     console.log("ONLYOFFICE CALLBACK CALLED", body);
-    if (body.status === 2 && body.url) {
+    // if (body.status === 2 && body.url) {
+    if ((body.status === 2 || body.status === 6) && body.url) {
       console.log("Saving document from onluyoffice:");
 
       const response = await axios.get(body.url, {
